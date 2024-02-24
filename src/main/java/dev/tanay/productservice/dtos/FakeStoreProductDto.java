f4ced864a3f4fbd7a3e21f3ff86e8cd53c1889c9
TanaySrivas <tanaysriofficial@gmail.com>
2024-02-24 15:43:25 +0530

    RestTemplateBuilder setup

---
+++ src/main/java/dev/tanay/productservice/dtos/FakeStoreProductDto.java
@@ -0,0 +1,11 @@
+package dev.tanay.productservice.dtos;
+
+public class FakeStoreProductDto {
+    private Long id;
+    private String title;
+    private double price;
+    private String category;
+    private String description;
+    private String image;
+
+}
--- src/main/java/dev/tanay/productservice/services/FakeStoreProductService.java
+++ src/main/java/dev/tanay/productservice/services/FakeStoreProductService.java
@@ -1,13 +1,23 @@
 package dev.tanay.productservice.services;
 
+import dev.tanay.productservice.dtos.FakeStoreProductDto;
 import dev.tanay.productservice.models.Product;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service("FakeStoreProductService")
 public class FakeStoreProductService implements ProductService{
 
+    private RestTemplate restTemplate;
+    private String getProductRequestURL = "https://fakestoreapi.com/products/{id}";
+    public FakeStoreProductService(RestTemplateBuilder restTemplateBuilder){
+        restTemplate = restTemplateBuilder.build();
+    }
     @Override
     public String getProductById(Long id) {
+//        RestTemplate restTemplate = restTemplateBuilder.build();
+        restTemplate.getForEntity(getProductRequestURL, FakeStoreProductDto.class, id);
         return "Fetched details for the product id: "+ id;
     }
 }
