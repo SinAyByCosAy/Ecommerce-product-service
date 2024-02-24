0b063356f2c3aa37d3cfb0718a937c51c1ee781f
TanaySrivas <tanaysriofficial@gmail.com>
2024-02-24 19:15:56 +0530

    DTO access

--- src/main/java/dev/tanay/productservice/dtos/FakeStoreProductDto.java
+++ src/main/java/dev/tanay/productservice/dtos/FakeStoreProductDto.java
@@ -1,5 +1,10 @@
 package dev.tanay.productservice.dtos;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
 public class FakeStoreProductDto {
     private Long id;
     private String title;
--- src/main/java/dev/tanay/productservice/services/FakeStoreProductService.java
+++ src/main/java/dev/tanay/productservice/services/FakeStoreProductService.java
@@ -3,6 +3,7 @@ package dev.tanay.productservice.services;
 import dev.tanay.productservice.dtos.FakeStoreProductDto;
 import dev.tanay.productservice.models.Product;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -17,7 +18,8 @@ public class FakeStoreProductService implements ProductService{
     @Override
     public String getProductById(Long id) {
 //        RestTemplate restTemplate = restTemplateBuilder.build();
-        restTemplate.getForEntity(getProductRequestURL, FakeStoreProductDto.class, id);
+        ResponseEntity<FakeStoreProductDto> response =
+                restTemplate.getForEntity(getProductRequestURL, FakeStoreProductDto.class, id);
         return "Fetched details for the product id: "+ id;
     }
 }
