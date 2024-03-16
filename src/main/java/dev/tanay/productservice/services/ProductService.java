ef6192577a809eaf2b7ee9941aa879979e81e418
TanaySrivas <tanaysriofficial@gmail.com>
2024-03-16 22:49:17 +0530

    excpetion handler in Controller

--- src/main/java/dev/tanay/productservice/services/ProductService.java
+++ src/main/java/dev/tanay/productservice/services/ProductService.java
@@ -1,13 +1,14 @@
 package dev.tanay.productservice.services;
 
 import dev.tanay.productservice.dtos.GenericProductDto;
+import dev.tanay.productservice.exceptions.NotFoundException;
 import dev.tanay.productservice.models.Product;
 
 import java.util.List;
 
 public interface ProductService {
 
-    GenericProductDto getProductById(Long id);
+    GenericProductDto getProductById(Long id) throws NotFoundException;
     GenericProductDto createProduct(GenericProductDto product);
     List<GenericProductDto> getAllProducts();
     GenericProductDto updateProductById(GenericProductDto product, Long id);
