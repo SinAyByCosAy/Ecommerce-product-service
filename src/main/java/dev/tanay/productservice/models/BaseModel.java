58c9c0ab59a18a0bb4ac2a7735a9b6dde861ce2c
TanaySrivas <tanaysriofficial@gmail.com>
2024-04-28 15:09:11 +0530

    Id to UUID change

--- src/main/java/dev/tanay/productservice/models/BaseModel.java
+++ src/main/java/dev/tanay/productservice/models/BaseModel.java
@@ -1,10 +1,15 @@
 package dev.tanay.productservice.models;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
+
+import java.util.UUID;
 
 @MappedSuperclass
 public class BaseModel {
     @Id
-    private Long id;
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID uuid;
 }
