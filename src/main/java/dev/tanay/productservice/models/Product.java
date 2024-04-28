44e94c85898307b50045206de60c8886627a9f80
TanaySrivas <tanaysriofficial@gmail.com>
2024-04-28 15:09:11 +0530

    Id to UUID change

--- src/main/java/dev/tanay/productservice/models/Product.java
+++ src/main/java/dev/tanay/productservice/models/Product.java
@@ -1,12 +1,18 @@
 package dev.tanay.productservice.models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
 
 @Entity
 public class Product extends BaseModel{
     private String title;
     private String description;
     private String image;
+//                   P : C
+//    L to R =>      1 : 1
+//    R to L =>      m : 1
+//    Cardinality => m : 1
+    @ManyToOne
     private Category category;
     //doubles have precisions issue, should be int. We are using double for FakeStore API
     private double price;
