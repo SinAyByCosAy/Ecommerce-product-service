b6e6e54dd2dc7ff0d6bb8e0f4e6cff9fda8044c9
SinAyByCosAy <tanaysriofficial@gmail.com>
2025-03-18 12:08:03 +0530

    Adapter refactor

--- src/main/java/dev/tanay/productservice/controllers/ProductController.java
+++ src/main/java/dev/tanay/productservice/controllers/ProductController.java
@@ -1,11 +1,8 @@
 package dev.tanay.productservice.controllers;
 
-import dev.tanay.productservice.dtos.ExceptionDto;
 import dev.tanay.productservice.dtos.GenericProductDto;
 import dev.tanay.productservice.exceptions.NotFoundException;
 import dev.tanay.productservice.services.ProductService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,45 +16,31 @@ import java.util.List;
 @RequestMapping("${app.base.url}")
 public class ProductController {
 
-//    @Autowired
     private ProductService productService;
-
     public ProductController(ProductService productService){
         this.productService = productService;
     }
-
-//Setter Injection
-//    @Autowired
-//    void setProductService(ProductService productService){
-//        this.productService = productService;
-//    }
-
-// localhost:8080//products/id
-// localhost:8080//products/123
-
     @GetMapping
     public List<GenericProductDto> getAllProducts(){
         return productService.getAllProducts();
     }
-
     @GetMapping("{id}")
-    public GenericProductDto getProductById(@PathVariable("id") Long id) throws NotFoundException {
+    public GenericProductDto getProductsById(@PathVariable Long id)throws NotFoundException {
         return productService.getProductById(id);
     }
-
     @PostMapping
     public GenericProductDto createProduct(@RequestBody GenericProductDto product){
         return productService.createProduct(product);
     }
-
-    @PutMapping("{id}")
-    public GenericProductDto updateProductById(@RequestBody GenericProductDto product, @PathVariable Long id){
-        return productService.updateProductById(product, id);
-    }
-
     @DeleteMapping("{id}")
-    public GenericProductDto deleteProductById(@PathVariable Long id){
-        return productService.deleteProductById(id);
+    public ResponseEntity<GenericProductDto> deleteProduct(@PathVariable Long id){
+        return new ResponseEntity<>(
+                productService.deleteProduct(id),
+                HttpStatus.NOT_FOUND
+        );
     }
-
+    @PutMapping("{id}")
+    public GenericProductDto updateProduct(@RequestBody GenericProductDto product, @PathVariable Long id){
+        return productService.updateProduct(product, id);
+    }
 }
