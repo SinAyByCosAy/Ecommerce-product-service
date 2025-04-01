28b7eeabc88e26203231f053f7c6dd9cf72eefcc
SinAyByCosAy <tanaysriofficial@gmail.com>
2025-04-01 12:42:55 +0530

    basic cardinality setup

--- src/main/java/dev/tanay/productservice/models/Category.java
+++ src/main/java/dev/tanay/productservice/models/Category.java
@@ -1,8 +1,14 @@
 package dev.tanay.productservice.models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+
+import java.util.List;
 
 @Entity
 public class Category extends BaseModel{
     private String name;
+    @OneToMany(fetch = jakarta.persistence.FetchType.EAGER, mappedBy = "category")
+    private List<Product> products;
+    //this is the same relation being mapped by category attribute in the other(Product) class
 }
