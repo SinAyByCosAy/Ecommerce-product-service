a815dfb7b035f75915efa9c4d77bf44693dda5df
SinAyByCosAy <tanaysriofficial@gmail.com>
2025-09-22 02:46:54 +0530

    Added repository test

--- src/main/java/dev/tanay/productservice/ProductserviceApplication.java
+++ src/main/java/dev/tanay/productservice/ProductserviceApplication.java
@@ -1,7 +1,12 @@
 package dev.tanay.productservice;
 
+import dev.tanay.productservice.models.Category;
+import dev.tanay.productservice.models.Product;
+import dev.tanay.productservice.repositories.ProductRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ProductserviceApplication {
@@ -10,4 +15,17 @@ public class ProductserviceApplication {
         SpringApplication.run(ProductserviceApplication.class, args);
     }
 
+    @Bean
+    CommandLineRunner run(ProductRepository productRepository){
+        return args -> {
+            Product product = new Product();
+            product.setTitle("book1");
+            product.setImage("https://kk.com/abc.png");
+            product.setDescription("Great book");
+            Category category = new Category();
+            category.setName("Books");
+            product.setCategory(category);
+            productRepository.save(product);
+        };
+    }
 }
