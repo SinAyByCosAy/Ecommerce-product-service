fccbab48a931dab8dba05fbbf0be8b1c2c9ba0c9
SinAyByCosAy <tanaysriofficial@gmail.com>
2025-09-28 13:05:27 +0530

    Dummy insert

--- src/main/java/dev/tanay/productservice/ProductserviceApplication.java
+++ src/main/java/dev/tanay/productservice/ProductserviceApplication.java
@@ -2,11 +2,14 @@ package dev.tanay.productservice;
 
 import dev.tanay.productservice.models.Category;
 import dev.tanay.productservice.models.Product;
+import dev.tanay.productservice.repositories.CategoryRepository;
 import dev.tanay.productservice.repositories.ProductRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
+import java.util.UUID;
 
 @SpringBootApplication
 public class ProductserviceApplication {
@@ -16,7 +19,7 @@ public class ProductserviceApplication {
     }
 
     @Bean
-    CommandLineRunner run(ProductRepository productRepository){
+    CommandLineRunner run(ProductRepository productRepository, CategoryRepository categoryRepository){
         return args -> {
             Product product = new Product();
             product.setTitle("book1");
@@ -26,6 +29,11 @@ public class ProductserviceApplication {
             category.setName("Books");
             product.setCategory(category);
             productRepository.save(product);
+
+
+            Category category1 = categoryRepository.findById(UUID.fromString("c078b998-fcbf-4976-8b24-665ba313a075")).get();
+            System.out.println(category1.getName());
+            for(Product prod : category1.getProducts()) System.out.println(prod.getTitle());
         };
     }
 }
