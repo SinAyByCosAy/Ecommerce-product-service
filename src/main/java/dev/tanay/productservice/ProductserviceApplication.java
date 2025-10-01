9e2180c6eea720ea2ca537a21ddb929f3ea28977
SinAyByCosAy <tanaysriofficial@gmail.com>
2025-10-01 19:03:06 +0530

    JPA queries

--- src/main/java/dev/tanay/productservice/ProductserviceApplication.java
+++ src/main/java/dev/tanay/productservice/ProductserviceApplication.java
@@ -1,6 +1,7 @@
 package dev.tanay.productservice;
 
 import dev.tanay.productservice.models.Category;
+import dev.tanay.productservice.models.Price;
 import dev.tanay.productservice.models.Product;
 import dev.tanay.productservice.repositories.CategoryRepository;
 import dev.tanay.productservice.repositories.ProductRepository;
@@ -21,19 +22,31 @@ public class ProductserviceApplication {
     @Bean
     CommandLineRunner run(ProductRepository productRepository, CategoryRepository categoryRepository){
         return args -> {
-            Product product = new Product();
-            product.setTitle("book1");
-            product.setImage("https://kk.com/abc.png");
-            product.setDescription("Great book");
-            Category category = new Category();
-            category.setName("Books");
-            product.setCategory(category);
-            productRepository.save(product);
+            //to save a new product
+//            Product product = new Product();
+//            product.setTitle("book2");
+//            product.setImage("https://kk.com/boop.png");
+//            product.setDescription("lengthy book");
+//            Category category = new Category();
+//            category.setName("Books");
+//            product.setCategory(category);
+//            Price price = new Price();
+//            price.setCurrency("ruppee");
+//            price.setPrice(22.2);
+//            product.setPrice(price);
+//            productRepository.save(product);
 
-
-            Category category1 = categoryRepository.findById(UUID.fromString("c078b998-fcbf-4976-8b24-665ba313a075")).get();
+            //replace the UUID with the one in the DB
+            Category category1 = categoryRepository.findById(UUID.fromString("125b5f4f-8c12-4bb7-80ce-b208b924b24e")).get();
             System.out.println(category1.getName());
             for(Product prod : category1.getProducts()) System.out.println(prod.getTitle());
+
+            String title = "book1";
+            System.out.println(productRepository.findByTitle(title).getTitle());
+
+            String title2 = "book2";
+            double price = 22.2;
+            System.out.println(productRepository.findByTitleAndPrice_Price(title2, price).getDescription());
         };
     }
 }
--- src/main/java/dev/tanay/productservice/repositories/ProductRepository.java
+++ src/main/java/dev/tanay/productservice/repositories/ProductRepository.java
@@ -1,11 +1,16 @@
 package dev.tanay.productservice.repositories;
 
 import dev.tanay.productservice.models.Product;
+import org.springframework.data.domain.Example;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.FluentQuery;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
+import java.util.function.Function;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, UUID> {
+    Product findByTitle(String title);
+    Product findByTitleAndPrice_Price(String title, double price);
 }
