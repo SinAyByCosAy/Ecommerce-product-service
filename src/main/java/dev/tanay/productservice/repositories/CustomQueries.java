dd89d9199d98232cf3df48fdbdc6e7bcaee5fdfd
SinAyByCosAy <tanaysriofficial@gmail.com>
2025-10-09 01:24:33 +0530

    Custom HQL query

--- src/main/java/dev/tanay/productservice/repositories/CustomQueries.java
+++ src/main/java/dev/tanay/productservice/repositories/CustomQueries.java
@@ -1,5 +1,5 @@
 package dev.tanay.productservice.repositories;
 
 public interface CustomQueries {
-    String FIND_ALL_BY_TITLE = "select * from product where title = :name";
+    String FIND_ALL_BY_TITLE = "select p from Product p where p.title = :name";
 }
--- src/main/java/dev/tanay/productservice/repositories/ProductRepository.java
+++ src/main/java/dev/tanay/productservice/repositories/ProductRepository.java
@@ -1,20 +1,17 @@
 package dev.tanay.productservice.repositories;
 
 import dev.tanay.productservice.models.Product;
-import org.springframework.data.domain.Example;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.FluentQuery;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.UUID;
-import java.util.function.Function;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, UUID> {
     Product findByTitle(String title);
     Product findByTitleAndPrice_Price(String title, double price);
-    @Query(value = CustomQueries.FIND_ALL_BY_TITLE, nativeQuery = true)
+    @Query(CustomQueries.FIND_ALL_BY_TITLE)
     List<Product> findAllByTitle(String name);
 }
