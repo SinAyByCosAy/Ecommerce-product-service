60680f103d9e931982e1e5ec5c05e34c3a3b7193
SinAyByCosAy <tanaysriofficial@gmail.com>
2026-02-22 13:34:31 +0530

    Added filter chain

---
+++ src/main/java/dev/tanay/productservice/security/SecurityConfig.java
@@ -0,0 +1,21 @@
+package dev.tanay.productservice.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable()) // Disable CSRF for non-browser clients (Postman/React)
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll() // Allow EVERYTHING
+                );
+
+        return http.build();
+    }
+}
