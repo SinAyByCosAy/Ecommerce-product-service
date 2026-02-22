3862307aa3ce45b99724eadeeaff4c5b10e6b890
SinAyByCosAy <tanaysriofficial@gmail.com>
2026-02-22 19:12:46 +0530

    JWT authentication linked to security config

--- src/main/java/dev/tanay/productservice/security/SecurityConfig.java
+++ src/main/java/dev/tanay/productservice/security/SecurityConfig.java
@@ -5,16 +5,20 @@ import org.springframework.context.annotation.Configuration;
 
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, TokenValidator tokenValidator) throws Exception {
+        JwtAuthenticationFilter filter = new JwtAuthenticationFilter(tokenValidator);
+
         http
                 .csrf(csrf -> csrf.disable()) // Disable CSRF for non-browser clients (Postman/React)
                 .authorizeHttpRequests(auth -> auth
                         .anyRequest().permitAll() // Allow EVERYTHING
-                );
+                )
+                .addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
