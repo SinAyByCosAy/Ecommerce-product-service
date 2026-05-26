a6bea6cf7c8476c03fe8a023a47710844f363116
SinAyByCosAy <tanaysriofficial@gmail.com>
2026-05-27 01:01:36 +0530

    Opensearch integration - base setup

---
+++ src/main/java/dev/tanay/productservice/models/ProductDocument.java
@@ -0,0 +1,20 @@
+package dev.tanay.productservice.models;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+
+@Document(indexName = "products")
+public class ProductDocument {
+    @Id
+    private String id;
+    @Field(type = FieldType.Text)
+    private String title;
+    @Field(type = FieldType.Text)
+    private String description;
+    @Field(type = FieldType.Text)
+    private Double price;
+    @Field(type = FieldType.Keyword)
+    private String brandName;
+}
