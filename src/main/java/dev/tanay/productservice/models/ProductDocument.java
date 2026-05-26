d6d288d29a1602c67241fae132432bc9f1091b62
SinAyByCosAy <tanaysriofficial@gmail.com>
2026-05-27 01:03:17 +0530

    Opensearch: doc

--- src/main/java/dev/tanay/productservice/models/ProductDocument.java
+++ src/main/java/dev/tanay/productservice/models/ProductDocument.java
@@ -1,10 +1,14 @@
 package dev.tanay.productservice.models;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.elasticsearch.annotations.Document;
 import org.springframework.data.elasticsearch.annotations.Field;
 import org.springframework.data.elasticsearch.annotations.FieldType;
 
+@Getter
+@Setter
 @Document(indexName = "products")
 public class ProductDocument {
     @Id
