e6eb9114c1eb81d855b97ea9efcad30fa6c926f9
SinAyByCosAy <tanaysriofficial@gmail.com>
2026-05-27 01:20:21 +0530

    Opensearch: service setup

---
+++ src/main/java/dev/tanay/productservice/services/ProductSearchService.java
@@ -0,0 +1,20 @@
+package dev.tanay.productservice.services;
+
+import dev.tanay.productservice.models.ProductDocument;
+import dev.tanay.productservice.repositories.ProductSearchRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.elasticsearch.core.SearchPage;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductSearchService {
+    private final ProductSearchRepository productSearchRepository;
+    public ProductSearchService(ProductSearchRepository productSearchRepository){
+        this.productSearchRepository = productSearchRepository;
+    }
+    private SearchPage<ProductDocument> searchProductDocument(String keyword, Pageable pageable){
+        if(keyword == null || keyword.trim().isEmpty()) return null;
+        return productSearchRepository.findByTitleMatchesOrDescriptionMatches(keyword, keyword, pageable);
+    }
+}
